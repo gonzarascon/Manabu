6ac50430361d4359e91cdef9df34ecbf8341ad7c
Gonza <gonzarascon@gmail.com>
2019-05-30 21:29:56 -0300

    Added .babelrc configuration

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,12 @@ import React, { PureComponent } from 'react';
 import App, { Container } from 'next/app';
 import Head from 'next/head';
 import { Grommet, Grid } from 'grommet';
+import styled from 'styled-components';
+
+const MainWrapper = styled.main`
+  width: 100%;
+  max-width: 1640px;
+`;
 
 const customTheme = {
   global: {
@@ -25,19 +31,20 @@ export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Grommet theme={customTheme}>
-        <Grid
-          fill
-          columns={{ count: 'fill', size: 'medium' }}
-          gap="medium"
-          maxWidth="1640px"
-          color="custom"
-        >
-          <Head>
-            <title>Manabu</title>
-          </Head>
-          <Component {...pageProps} />
-        </Grid>
+      <Grommet theme={customTheme} full plain>
+        <MainWrapper>
+          <Grid
+            fill
+            columns={{ count: 'fill', size: 'medium' }}
+            gap="medium"
+            color="custom"
+          >
+            <Head>
+              <title>Manabu</title>
+            </Head>
+            <Component {...pageProps} />
+          </Grid>
+        </MainWrapper>
       </Grommet>
     );
   }
